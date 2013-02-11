616ba2b39b0147ee2b40efb8455e9038e994c549
panurg <panurg@bk.ru>
2013-02-11 22:39:47 +0400

    Fixed some resolution problems
    1. Пофиксил скрипт для работы с любым разрешением экрана. Проверено на
    1680x1050 и 1280x960
    2. Поправил вывод, добавил вывод числа попыток

--- wiz.py
+++ wiz.py
@@ -1,5 +1,5 @@
 # wiz
-# v0.1
+# v0.2
 # by panurg
 #
 # i hope u will enjoy this script :3
@@ -14,17 +14,32 @@ bp_needed = 40
 
 max = 65535
 
-gnome_x = max * 4 / 7
-gnome_y = max * 6 / 7
+width = win32api.GetSystemMetrics(0)
+height = win32api.GetSystemMetrics(1)
 
-cancel_x = max * 4 / 7
-cancel_y = max * 19 / 20
+font_size = 20
 
-ok_x = max * 3 / 7
-ok_y = max * 19 / 20
+race_button_width = 147
+race_button_height = 37
+small_button_width = 115
+small_button_height = 22
+race_split_interval = 7
+bottom_margin = 30
+bottom_split_interval = 38
+interval_between_small_buttons = 46
+center_ok_shift = 37
+
+gnome_x = max / 2 + max / width * (race_button_width + race_split_interval) / 2
+gnome_y = max - max / height * (bottom_margin + small_button_height + bottom_split_interval + race_button_height / 2)
+
+cancel_x = max / 2 + max / width * (small_button_width + interval_between_small_buttons) / 2
+cancel_y = max - max / height * (bottom_margin + small_button_height / 2)
+
+ok_x = max / 2 - max / width * (small_button_width + interval_between_small_buttons) / 2
+ok_y = cancel_y
 
 center_ok_x = max / 2
-center_ok_y = max * 11 / 20
+center_ok_y = max / 2 + max / height * center_ok_shift
 
 time.sleep(5)
 
@@ -53,20 +68,13 @@ def select_center_ok():
   win32api.mouse_event(0x8001, center_ok_x, center_ok_y)
   press()
 
-#win32api.mouse_event(0x8001, gnome_x, gnome_y)
-#win32api.mouse_event(0x8001, cancel_x, cancel_y)
-#win32api.mouse_event(0x8001, ok_x, ok_y)
-#win32api.mouse_event(0x8001, center_ok_x, center_ok_y)
-
 bonus_points = 0
-box = (490, 365, 540, 395)
+count = 0
+box = (width / 2 - font_size, height / 2 - font_size, width / 2 + font_size, height / 2 + font_size)
 api = tesseract.TessBaseAPI()
 api.Init(".","eng",tesseract.OEM_DEFAULT)
 api.SetVariable("tessedit_char_whitelist", "0123456789")
 api.SetPageSegMode(tesseract.PSM_SINGLE_WORD)
-
-#file = "screen.jpg"
-
 
 while bonus_points < bp_needed:
   select_cancel()
@@ -74,18 +82,17 @@ while bonus_points < bp_needed:
   select_ok()
   time.sleep(2.2)
   im = ImageGrab.grab(box)
-  #im.save(file)
-  #buffer = open(file, "rb").read()
-  #result = tesseract.ProcessPagesBuffer(buffer, len(buffer), api)
   cimg = cv.CreateImageHeader(im.size, cv.IPL_DEPTH_8U, 3)
   cv.SetData(cimg, im.tostring())
   tesseract.SetCvImage(cimg, api)
   result = api.GetUTF8Text()
-  print "rolled points = ", result
   if result != "":
     bonus_points = int(result)
   select_center_ok()
   if bonus_points > 70:
     bonus_points = 0
+  print "rolled points = ", bonus_points
+  count += 1
 
 print "Gratz!"
+print "number of attempts: ", count
